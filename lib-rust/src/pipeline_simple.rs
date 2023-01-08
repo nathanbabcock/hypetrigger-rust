2ca3aee150c2b2a4f43def058a8285a278f34eeb
nbabcock <nathan.r.babcock@gmail.com>
2023-01-08 14:05:04 -0600

    refactor: add wrapper to run Triggers on separate thread(s)

--- lib-rust/src/pipeline_simple.rs
+++ lib-rust/src/pipeline_simple.rs
@@ -1,6 +1,12 @@
 use crate::tesseract::init_tesseract;
 use photon_rs::PhotonImage;
-use std::sync::Mutex;
+use std::{
+    sync::{
+        mpsc::{Receiver, SyncSender},
+        Arc, Mutex,
+    },
+    thread::JoinHandle,
+};
 use tesseract::Tesseract;
 
 //// Image processing
@@ -42,7 +48,18 @@ struct Frame {
 
 //// Triggers
 pub trait Trigger {
-    fn run(&self, frame: Frame) -> Result<(), String>;
+    fn on_frame(&self, frame: Frame) -> Result<(), String>;
+
+    /// Convert this Trigger into a ThreadTrigger, running on a separate thread.
+    fn run_on_thread(self, runner_thread: RunnerThread) -> ThreadTrigger
+    where
+        Self: Sized + Send + Sync + 'static,
+    {
+        ThreadTrigger {
+            trigger: Arc::new(self),
+            runner_thread,
+        }
+    }
 }
 
 //// Tesseract
@@ -53,9 +70,56 @@ pub struct TesseractTrigger {
 }
 
 impl Trigger for TesseractTrigger {
-    fn run(&self, frame: Frame) -> Result<(), String> {
+    fn on_frame(&self, frame: Frame) -> Result<(), String> {
         Err("not implemented".to_string())
     }
+}
+
+//// "Threaded Triggers"
+
+/// A wrapper around any other Trigger that sends it across a channel to run on
+/// a separate thread.
+pub struct ThreadTrigger {
+    trigger: Arc<dyn Trigger + Send + Sync>,
+    runner_thread: RunnerThread,
+}
+
+impl Trigger for ThreadTrigger {
+    fn on_frame(&self, frame: Frame) -> Result<(), String> {
+        match self.runner_thread.tx.send(RunnerPayload {
+            frame,
+            trigger: self.trigger.clone(),
+        }) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(e.to_string()),
+        }
+    }
+}
+
+/// A separate thread that runs one or more ThreadedTriggers,
+/// by receiving them over a channel, paired with the frame to process.
+pub struct RunnerThread {
+    // rx: Receiver<RunnerPayload>,
+    pub tx: SyncSender<RunnerPayload>,
+    pub join_handle: JoinHandle<()>,
+}
+
+impl RunnerThread {
+    pub fn spawn(buffer_size: usize) -> Self {
+        let (tx, rx) = std::sync::mpsc::sync_channel::<RunnerPayload>(buffer_size);
+        let join_handle = std::thread::spawn(move || {
+            while let Ok(payload) = rx.recv() {
+                payload.trigger.on_frame(payload.frame);
+            }
+        });
+        Self { tx, join_handle }
+    }
+}
+
+/// Everything a RunnerThread needs to run a ThreadedTrigger
+pub struct RunnerPayload {
+    frame: Frame,
+    trigger: Arc<dyn Trigger + Send + Sync>,
 }
 
 //// Pipeline
@@ -135,6 +199,39 @@ pub fn _main() -> Result<(), String> {
             threshold: 42,
         },
     };
+
+    Hypetrigger::new()
+        .set_ffmpeg_exe("ffmpeg".to_string())
+        .set_input("test.mp4".to_string())
+        .set_fps(2)
+        .add_trigger(Box::new(trigger))
+        .run()
+}
+
+pub fn _main_threaded() -> Result<(), String> {
+    let tesseract = Mutex::new(match init_tesseract() {
+        Ok(tesseract) => tesseract,
+        Err(e) => return Err(e.to_string()),
+    });
+
+    let runner_thread = RunnerThread::spawn(1);
+
+    let trigger = TesseractTrigger {
+        tesseract,
+        crop: Crop {
+            left_percent: 0.0,
+            top_percent: 0.0,
+            width_percent: 100.0,
+            height_percent: 100.0,
+        },
+        threshold_filter: ThresholdFilter {
+            r: 255,
+            g: 255,
+            b: 255,
+            threshold: 42,
+        },
+    }
+    .run_on_thread(runner_thread);
 
     Hypetrigger::new()
         .set_ffmpeg_exe("ffmpeg".to_string())
