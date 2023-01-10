cb9cf6ae8854b03a9c1cb2ab541f7ddce3db8c54
nbabcock <nathan.r.babcock@gmail.com>
2023-01-10 09:37:58 -0600

    refactor: use scoped thread for ffmpeg_stderr

--- lib-rust/src/pipeline_simple.rs
+++ lib-rust/src/pipeline_simple.rs
@@ -1,4 +1,3 @@
-use crate::tesseract::init_tesseract;
 use image::RgbImage;
 use photon_rs::PhotonImage;
 use regex::Regex;
@@ -11,6 +10,8 @@ use std::path::{Path, PathBuf};
 use std::process::ChildStderr;
 use std::sync::mpsc::channel;
 use std::thread;
+use std::thread::Scope;
+use std::thread::ScopedJoinHandle;
 use std::{
     io::{self, BufRead},
     process::{Child, Command, Stdio},
@@ -220,7 +221,7 @@ impl Hypetrigger {
         };
 
         // Extract each stdio channel to use in different places
-        let ffmpeg_stderr = match ffmpeg_child.stderr.take() {
+        let mut ffmpeg_stderr = match ffmpeg_child.stderr.take() {
             Some(ffmpeg_stderr) => ffmpeg_stderr,
             None => return Err("no stderr".to_string()),
         };
@@ -233,51 +234,58 @@ impl Hypetrigger {
             None => return Err("no stdin".to_string()),
         };
 
-        // Spawn a thread to read stderr from ffmpeg
-        let (output_size_rx, ffmpeg_stderr_join_handle) =
-            match self.spawn_ffmpeg_stderr_thread(ffmpeg_stderr) {
-                Ok(ffmpeg_stderr_thread) => ffmpeg_stderr_thread,
-                Err(e) => {
-                    ffmpeg_child
-                        .kill()
-                        .expect("able to stop ffmpeg process if something goes wrong");
-                    return Err(e.to_string());
+        // Enter a new scope that will block until ffmpeg_stderr_thread is done
+        thread::scope(|scope| {
+            // Spawn a thread to read stderr from ffmpeg
+            let (output_size_rx, ffmpeg_stderr_join_handle) =
+                match self.spawn_ffmpeg_stderr_thread(&mut ffmpeg_stderr, scope) {
+                    Ok(ffmpeg_stderr_thread) => ffmpeg_stderr_thread,
+                    Err(e) => {
+                        ffmpeg_child
+                            .kill()
+                            .expect("able to stop ffmpeg process if something goes wrong");
+                        return Err(e.to_string());
+                    }
+                };
+
+            // Block on each line of ffmpeg stderr until receiving the output size
+            let (output_width, output_height) = output_size_rx.recv().map_err(|_| {
+            "ffmpeg exited before sending output size. This is likely due to an invalid input file.".to_string()
+          })?;
+            println!(
+                "[ffmpeg] Parsed output size from logs: {}x{}",
+                output_width, output_height
+            );
+
+            // Initialize a buffer
+            const CHANNELS: u32 = 3; // matches in the `-f rgb24` flag to ffmpeg
+            let buf_size = (output_width * output_height * CHANNELS) as usize;
+            let mut buffer = vec![0_u8; buf_size];
+            println!("[ffmpeg.stdout] Allocated buffer of size {}", buf_size);
+
+            // Read from stdout on the current thread, invoking Triggers each frame
+            let mut frame_num = 0;
+            while ffmpeg_stdout.read_exact(&mut buffer).is_ok() {
+                let image = match RgbImage::from_vec(output_width, output_height, buffer.clone()) {
+                    Some(image) => image,
+                    None => {
+                        return Err(
+                            "unable to convert vec to imagebuffer (size mismatch)".to_string()
+                        )
+                    }
+                };
+                let frame = Frame {
+                    image,
+                    frame_num,
+                    timestamp: frame_num as f64 / self.fps as f64,
+                };
+                for trigger in &self.triggers {
+                    trigger.on_frame(&frame);
                 }
-            };
-
-        // Block on each line of ffmpeg stderr until receiving the output size
-        let (output_width, output_height) = output_size_rx.recv().map_err(|_| {
-          "ffmpeg exited before sending output size. This is likely due to an invalid input file.".to_string()
-        })?;
-        println!(
-            "[ffmpeg] Parsed output size from logs: {}x{}",
-            output_width, output_height
-        );
-
-        // Initialize a buffer
-        const CHANNELS: u32 = 3; // implicit in the `-f rgb8` flag to ffmpeg
-        let buf_size = (output_width * output_height * CHANNELS) as usize;
-        let mut buffer = vec![0_u8; buf_size];
-        println!("[ffmpeg.stdout] Allocated buffer of size {}", buf_size);
-
-        // Read from stdout on the current thread, invoking Triggers each frame
-        let mut frame_num = 0;
-        while ffmpeg_stdout.read_exact(&mut buffer).is_ok() {
-            let image = match RgbImage::from_vec(output_width, output_height, buffer.clone()) {
-                Some(image) => image,
-                None => {
-                    return Err("unable to convert vec to imagebuffer (size mismatch)".to_string())
-                }
-            };
-            let frame = Frame {
-                image,
-                frame_num,
-                timestamp: frame_num as f64 / self.fps as f64,
-            };
-            for trigger in &self.triggers {
-                trigger.on_frame(&frame);
             }
-        }
+            println!("[ffmpeg.out] Finished reading from stdout");
+            Ok(())
+        })?;
 
         // Block until ffmpeg finishes
         let ffmpeg_exit_status = match ffmpeg_child.wait() {
@@ -359,10 +367,14 @@ impl Hypetrigger {
     /// sent back to the main thread via a channel. After the metadata is
     /// received, the channel closes, while the stderr handler thread continues
     /// to run in the background. It automatically stops after ffmpeg exits.
-    pub fn spawn_ffmpeg_stderr_thread(
-        &self,
-        ffmpeg_stderr: ChildStderr,
-    ) -> io::Result<(Receiver<(u32, u32)>, JoinHandle<Result<(), String>>)> {
+    pub fn spawn_ffmpeg_stderr_thread<'scope>(
+        &'scope self,
+        ffmpeg_stderr: &'scope mut ChildStderr,
+        scope: &'scope Scope<'scope, '_>, // scope scope scope scope wheeee
+    ) -> io::Result<(
+        Receiver<(u32, u32)>,
+        ScopedJoinHandle<'scope, Result<(), String>>,
+    )> {
         let (output_size_tx, output_size_rx) = channel::<(u32, u32)>();
         let thread_body = move || {
             let mut reader = BufReader::new(ffmpeg_stderr);
@@ -409,7 +421,7 @@ impl Hypetrigger {
 
         let join_handle = thread::Builder::new()
             .name("ffmpeg_stderr".to_string())
-            .spawn(thread_body)?;
+            .spawn_scoped(scope, thread_body)?;
 
         Ok((output_size_rx, join_handle))
     }
