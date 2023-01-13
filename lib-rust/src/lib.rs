a6a31cbcc08a0dd301154d166b25b9bd996d6def
nbabcock <nathan.r.babcock@gmail.com>
2023-01-13 15:16:07 -0600

    feat: add feature for wasm build and exclude unnecessary deps

--- lib-rust/src/lib.rs
+++ lib-rust/src/lib.rs
@@ -1,26 +1,46 @@
+#[cfg(not(feature = "wasm"))]
 #[macro_use]
 extern crate lazy_static;
 
+//// Image processing modules
+// Required for tesseract/tensorflow, but can be skipped for simple or custom
+// triggers. Also required for wasm builds and provides a rich image library.
 #[cfg(feature = "photon")]
 pub mod photon;
 
 #[cfg(feature = "photon")]
 pub mod threshold;
 
-#[cfg(feature = "tensorflow")]
-pub mod tensorflow;
-
+//// Specific trigger implementations (tesseract, tensorflow)
 #[cfg(feature = "tesseract")]
 pub mod tesseract;
 
+#[cfg(feature = "tensorflow")]
+pub mod tensorflow;
+
+//// Tests
 #[cfg(test)]
 mod tests;
 
+//// Core functionality
+// Not WASM-safe; intended for Rust usage only. Involves spawning and attaching to ffmpeg processes.
+#[cfg(not(feature = "wasm"))]
 pub mod async_trigger;
+
+#[cfg(not(feature = "wasm"))]
 pub mod debug;
+
+#[cfg(not(feature = "wasm"))]
 pub mod error;
-pub mod logging;
+
+#[cfg(not(feature = "wasm"))]
 pub mod pipeline;
+
+#[cfg(not(feature = "wasm"))]
 pub mod simple_trigger;
+
+#[cfg(not(feature = "wasm"))]
 pub mod trigger;
+
+#[cfg(not(feature = "wasm"))]
 pub mod util;
