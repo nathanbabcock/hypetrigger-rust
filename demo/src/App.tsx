3f2b226fe8b28438f8e81122069206ccd1e1f6ad
nbabcock <nathan.r.babcock@gmail.com>
2023-01-16 15:19:22 -0600

    feat: create signals and effects for canvas demo

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,4 +1,4 @@
-import { createEffect } from 'solid-js'
+import { createEffect, createMemo, createSignal } from 'solid-js'
 import { Hypetrigger, initWasm } from '../../lib-js/src'
 import {
   initTesseractScheduler,
@@ -6,93 +6,106 @@ import {
 } from './../../lib-js/src/tesseract'
 
 export default function App() {
-  initWasm()
-
   let canvas: HTMLCanvasElement | undefined
+  const ctx = () => canvas?.getContext('2d', { willReadFrequently: true })
+
+  const [mousedown, setMousedown] = createSignal(false)
+  const [mousePos, setMousePos] = createSignal<
+    { x: number; y: number } | undefined
+  >()
+  const [penSize, setPenSize] = createSignal(5)
+
+  const [yourText, setYourText] = createSignal<string | undefined>()
+  const responseText = () =>
+    yourText()?.toLowerCase().includes('hello')
+      ? 'Hello to you too!'
+      : undefined
+
+  createEffect(() =>
+    console.log({ yourText: yourText(), responseText: responseText() })
+  )
+
+  const startDrawing = (e: Event) => {
+    setMousedown(true)
+    e.preventDefault()
+  }
+
+  const stopDrawing = (e: Event) => {
+    setMousedown(false)
+    e.preventDefault()
+  }
+
+  const render = () =>
+    requestAnimationFrame(() => {
+      if (mousedown() && mousePos()) {
+        ctx().fillStyle = 'cornflowerblue'
+        ctx().moveTo(mousePos().x, mousePos().y)
+        ctx().ellipse(
+          mousePos().x,
+          mousePos().y,
+          penSize(),
+          penSize(),
+          0,
+          0,
+          2 * Math.PI
+        )
+        ctx().fill()
+      }
+      render()
+    })
+
+  const init = async () => {
+    await initWasm()
+    const scheduler = await initTesseractScheduler({ numWorkers: 1 })
+    const trigger = new TesseractTrigger(scheduler)
+    new Hypetrigger(canvas).addTrigger(trigger).runRealtime()
+    trigger.onText = text => setYourText(text)
+  }
+
+  createEffect(() => {
+    if (!canvas) return
+    init()
+    render()
+  })
+
   return (
     <>
-      <canvas ref={canvas} id="canvas" width="500" height="500"></canvas>
+      <canvas
+        ref={canvas}
+        id="canvas"
+        width="500"
+        height="500"
+        onMouseMove={e => setMousePos({ x: e.offsetX, y: e.offsetY })}
+        onTouchMove={e =>
+          setMousePos({
+            x: e.touches[0].clientX - canvas.offsetLeft,
+            y: e.touches[0].clientY - canvas.offsetTop,
+          })
+        }
+        onMouseDown={startDrawing}
+        onMouseUp={stopDrawing}
+        onMouseOut={stopDrawing}
+        onTouchStart={startDrawing}
+        onTouchEnd={stopDrawing}
+        onTouchCancel={stopDrawing}
+      ></canvas>
       <div id="right-col">
         <div id="welcome">
           <h1>👈 Say Hello to Hypetrigger.</h1>
           <p>
-            Draw swords on the canvas to the left to test how well Hypetrigger
+            Draw words on the canvas to the left to test how well Hypetrigger
             can recognize text in realtime.
           </p>
         </div>
-        <div id="recognizedText">
+        <div id="your-wrapper" class={!yourText() ? 'hidden' : ''}>
           <span id="your-label">You wrote:</span>
-          <code id="your-text">asdf</code>
+          <code id="your-text">{yourText()}</code>
         </div>
-        <div id="response">
-          <code id="hypetrigger-msg">Hello, nice to meet you!</code>
-          <span id="hypetrigger-label">- Hypetrigger</span>
+        <div id="response-wrapper" class={!responseText() ? 'hidden' : ''}>
+          <code id="response-text">{responseText()}</code>
+          <span id="response-label">- Hypetrigger</span>
         </div>
       </div>
     </>
   )
 }
-
-// const canvas = document.getElementById('canvas') as HTMLCanvasElement
-// const ctx = canvas.getContext('2d', { willReadFrequently: true })!
-// ctx.fillStyle = 'cornflowerblue'
-
-// const state = {
-//   mousedown: false,
-//   mouseX: undefined as number | undefined,
-//   mouseY: undefined as number | undefined,
-//   penSize: 5,
-// }
-
-// canvas.addEventListener('mousemove', e => {
-//   state.mouseX = e.offsetX
-//   state.mouseY = e.offsetY
-// })
-// canvas.addEventListener('touchmove', e => {
-//   state.mouseX = e.touches[0].clientX - canvas.offsetLeft
-//   state.mouseY = e.touches[0].clientY - canvas.offsetTop
-// })
-// const startDrawing = (e: Event) => {
-//   state.mousedown = true
-//   e.preventDefault()
-// }
-// const stopDrawing = (e: Event) => {
-//   state.mousedown = false
-//   e.preventDefault()
-// }
-// canvas.addEventListener('mousedown', startDrawing)
-// canvas.addEventListener('mouseup', stopDrawing)
-// canvas.addEventListener('mouseout', stopDrawing)
-// canvas.addEventListener('touchstart', startDrawing)
-// canvas.addEventListener('touchend', stopDrawing)
-// canvas.addEventListener('touchcancel', stopDrawing)
-// const render = () =>
-//   requestAnimationFrame(() => {
-//     if (
-//       state.mousedown &&
-//       state.mouseX !== undefined &&
-//       state.mouseY !== undefined
-//     ) {
-//       ctx.moveTo(state.mouseX, state.mouseY)
-//       ctx.ellipse(
-//         state.mouseX,
-//         state.mouseY,
-//         state.penSize,
-//         state.penSize,
-//         0,
-//         0,
-//         2 * Math.PI
-//       )
-//       ctx.fill()
-//     }
-//     render()
-//   })
-// render()
-
-// const scheduler = await initTesseractScheduler()
-// const trigger = new TesseractTrigger(scheduler)
-// new Hypetrigger(canvas).addTrigger(trigger).runRealtime()
-// const recognizedText = document.getElementById(
-//   'recognizedText'
-// ) as HTMLDivElement
-// trigger.onText = text => (recognizedText.innerText = text)
