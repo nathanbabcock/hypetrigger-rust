21b08b7f4ec37367e7f0870d243fe32d3ee54d27
nbabcock <nathan.r.babcock@gmail.com>
2023-01-17 12:52:15 -0600

    feat: add tesseract debug option

--- lib-rust/src/tesseract.rs
+++ lib-rust/src/tesseract.rs
@@ -24,10 +24,20 @@ pub struct TesseractResult {
 
 #[derive(Clone)]
 pub struct TesseractTrigger {
+    /// The initialized instance of Tesseract that will be used to run this trigger
     pub tesseract: TesseractRef,
+
+    /// The region to crop to before running OCR.
     pub crop: Option<Crop>,
+
+    /// The threshold filter to apply before running OCR.
     pub threshold_filter: Option<ThresholdFilter>,
+
+    /// The callback to run after OCR is complete.
     pub callback: Option<TesseractTriggerCallback>,
+
+    /// Pause execution after each step of image pre-processing.
+    pub enable_debug_breakpoints: bool,
 }
 
 impl Trigger for TesseractTrigger {
@@ -57,9 +67,7 @@ impl Trigger for TesseractTrigger {
 
 impl TesseractTrigger {
     pub fn preprocess_image(&self, mut image: PhotonImage) -> Result<PhotonImage> {
-        /// If `true`, pauses execution after each step of image pre-processing.
-        const DEBUG: bool = false;
-        if DEBUG {
+        if self.enable_debug_breakpoints {
             println!("[tesseract] received frame");
             debug_photon_image(&image)?;
         }
@@ -68,7 +76,7 @@ impl TesseractTrigger {
         if let Some(crop) = &self.crop {
             image = crop.apply(image);
         }
-        if DEBUG {
+        if self.enable_debug_breakpoints {
             println!("[tesseract] cropped");
             debug_photon_image(&image)?;
         }
@@ -76,7 +84,7 @@ impl TesseractTrigger {
         // Minimum size
         const MIN_TESSERACT_IMAGE_SIZE: u32 = 32;
         image = ensure_minimum_size(&image, MIN_TESSERACT_IMAGE_SIZE);
-        if DEBUG {
+        if self.enable_debug_breakpoints {
             println!("[tesseract] resized");
             debug_photon_image(&image)?;
         }
@@ -85,7 +93,7 @@ impl TesseractTrigger {
         if let Some(filter) = &self.threshold_filter {
             image = filter.apply(image);
         }
-        if DEBUG {
+        if self.enable_debug_breakpoints {
             println!("[tesseract] filtered");
             debug_photon_image(&image)?;
         }
@@ -93,7 +101,7 @@ impl TesseractTrigger {
         // Padding
         let padding_bg: Rgba = Rgba::new(255, 255, 255, 255);
         image = padding_uniform(&image, MIN_TESSERACT_IMAGE_SIZE, padding_bg);
-        if DEBUG {
+        if self.enable_debug_breakpoints {
             println!("[tesseract] padded (done)");
             debug_photon_image(&image)?;
         }
@@ -182,63 +190,3 @@ where
     let tesseract = Tesseract::new(Some(datapath), Some(language))?;
     Ok(Arc::new(Mutex::new(Some(tesseract))))
 }
-
-// /// Either apply a filter or pass through the original image
-// pub fn try_filter(image: PhotonImage, filter: Option<ThresholdFilter>) -> PhotonImage {
-//     match filter {
-//         Some(filter) => {
-//             let color = Rgb::new(filter.r, filter.g, filter.b);
-//             threshold_color_distance(image, &color, filter.threshold)
-//         }
-//         _ => image,
-//     }
-// }
-
-// /// 1. (optional) Resize if smaller than 32px in either dimension
-// /// 2. (optional) Apply threshold filter if present
-// /// 3. Add 32px padding to all sides for better OCR results
-// #[wasm_bindgen]
-// pub fn preprocess_image_for_tesseract(
-//     image: &PhotonImage,
-//     filter: Option<ThresholdFilter>,
-// ) -> PhotonImage {
-//     // Tesseract struggles to recognize text below this size.
-//     const MIN_TESSERACT_IMAGE_SIZE: u32 = 32;
-
-//     // Color for background padding
-//     let background: Rgba = Rgba::new(255, 255, 255, 255);
-
-//     // Chain image transformations
-//     // NB: Is there a fancy pipe() + partial application operator for this? It would
-//     // need to bind the output of one function to the input of the next, while
-//     // allowing additional subsequent arguments.
-//     let mut new_image: PhotonImage;
-//     new_image = ensure_minimum_size(image, MIN_TESSERACT_IMAGE_SIZE);
-//     new_image = try_filter(new_image, filter);
-//     new_image = padding_uniform(&new_image, MIN_TESSERACT_IMAGE_SIZE, background);
-//     new_image
-// }
-
-// /// Recognize text from an image
-// pub fn ocr(image: PhotonImage, tesseract: &RefCell<Option<Tesseract>>) -> String {
-//     let _now = Instant::now();
-//     let rgba32 = image.get_raw_pixels();
-//     let buf = rgba32.as_slice();
-//     let channels = 4;
-
-//     let mut model = tesseract.replace(None).unwrap();
-//     model = model
-//         .set_frame(
-//             buf,
-//             image.get_width() as i32,
-//             image.get_height() as i32,
-//             channels,
-//             image.get_width() as i32 * channels,
-//         )
-//         .expect("load image from memory")
-//         .set_source_resolution(96);
-//     let result = model.get_text().expect("get text");
-//     tesseract.replace(Some(model));
-
-//     result
-// }
